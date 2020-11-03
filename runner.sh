3b768662918e1c4c8cb2c780d7178bfe6ba793af
H@di <hadi@hadisafari.ir>
2020-11-03 19:43:22 +0330

    add runner

---
+++ runner.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# RNUNNE="./test3.py"
+# PREF="ga"
+
+RNUNNE="./test4.py"
+PREF="sa"
+
+N=10
+
+# set -o xtrace
+
+echo "run $RNUNNE and write in $PREF"
+
+[ -d $PREF ] && rm --interactive=once -r $PREF
+[ -d $PREF ] && exit 1
+mkdir $PREF
+
+for i in $(seq $N); do
+    echo ""
+    echo -e "\e[96mrun $i\e[0m"
+    echo -e "\e[36m======\e[0m"
+    time $RNUNNE $PREF/$i;
+done
